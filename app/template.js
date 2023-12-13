1fbf3fd75cd12fdfad102c80e1fa88575b50da1e
rickyclarke <rickyclarke@bitbucket.org>
2023-12-13 16:34:44 +0000

    Edit logo

--- app/template.js
+++ app/template.js
@@ -12,13 +12,14 @@ export default function Template({ children }) {
               <div>
                     <header>
                     <div className="container py-5 flex items-center justify-between">
-                        <a className='logo' href="/">
+                        <a className='logo flex items-center gap-5' href="/">
                             <Image 
                             src="/headless.png" 
                             alt="Logo" 
                             width={70}
                             height={70}
                             priority />
+                            <span>Headless WordPress site</span>
                         </a>
                         <PrimaryMenu />
                     </div>
@@ -30,7 +31,7 @@ export default function Template({ children }) {
           
             <footer>
                 <div className="container py-5">
-                    <p>&copy; {year} Ricky Clarke</p>
+                    <p className='m-0'>&copy; {year} Ricky Clarke</p>
                 </div>
             </footer>
         </>
