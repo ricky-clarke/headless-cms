db6968c3cb36cfe35045ae564a3a4993d1c7667e
rickyclarke <rickyclarke@bitbucket.org>
2023-12-13 15:05:10 +0000

    Add logo and edit footer

--- app/template.js
+++ app/template.js
@@ -1,14 +1,24 @@
 import PrimaryMenu from './components/primary_menu/primary_menu.component';
+import Image from 'next/image';
 import '../app/globals.css';
 
 export default function Template({ children }) {
+
+    const d = new Date();
+    let year = d.getFullYear();
 
     return (
         <>
               <div>
                     <header>
                     <div className="container py-5 flex items-center justify-between">
-                        <a className='logo' href="/">HEADLESS</a>
+                        <a className='logo' href="/">
+                            <Image 
+                            src="/headless.png" 
+                            alt="Logo" 
+                            width={70}
+                            height={70}/>
+                        </a>
                         <PrimaryMenu />
                     </div>
                     </header>
@@ -19,7 +29,7 @@ export default function Template({ children }) {
           
             <footer>
                 <div className="container py-5">
-                    <p>Footer</p>
+                    <p>&copy; {year} Ricky Clarke</p>
                 </div>
             </footer>
         </>
