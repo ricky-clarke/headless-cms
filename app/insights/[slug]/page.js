c213489571153d7052c71dc53d61bf2c8637f737
rickyclarke <rickyclarke@bitbucket.org>
2024-05-09 08:47:21 +0100

    Remove ACF loop for insights - fix deployment issue test

--- app/insights/[slug]/page.js
+++ app/insights/[slug]/page.js
@@ -1,6 +1,7 @@
 import styles from '../insight-post.module.scss'
 import Link from "next/link";
 import '../../globals.scss';
+
 // Dynamic meta data
 export async function generateMetadata({ params }, parent) {
     const slug = params.slug
@@ -13,25 +14,26 @@ export async function generateMetadata({ params }, parent) {
   
   }
  
-export default async function SinglePost ( { params} ) {
+export default async function SinglePost () {
 
-    const getPostData = async () => {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_HEADLESS_API}posts?slug=${params.slug}&acf_format=standard`);
-        return res.json();
-      }
+    // const getPostData = async () => {
+    //     const res = await fetch(`${process.env.NEXT_PUBLIC_HEADLESS_API}posts?slug=${params.slug}&acf_format=standard`);
+    //     return res.json();
+    //   }
     
-    const data = await getPostData();  
+    // const data = await getPostData();  
 
     return (
         <>
                 <div className={`${styles.insight_post} container header--bg`}>
                     <div className="mt-[9em]">
                         <Link href="/insights" className={`${styles.insight_post_back} mb-[5em] inline-block`}> ALL INSIGHTS</Link>
-                        <h1>{data[0]?.title.rendered}</h1>
+                        <h1>INISIGHT</h1>
+                        {/* <h1>{data[0]?.title.rendered}</h1>
                         <div className="md:w-3/5">
                             <div className="mt-3 mb-8" dangerouslySetInnerHTML={{ __html: data[0].excerpt.rendered }}></div>
                             <div dangerouslySetInnerHTML={{ __html: data[0]?.content.rendered }}></div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
         </>
